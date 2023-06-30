d102df27e6ce3c7922d39c4341fcc4e4d50137c5
Paul M. Jones <pmjones@pmjones.io>
2023-06-30 13:26:08 -0500

    declare strict_types throughout

--- src/SapienHelperMethods.php
+++ src/SapienHelperMethods.php
@@ -5,13 +5,13 @@ namespace Qiq\Helper\Sapien;
 
 trait SapienHelperMethods
 {
-	public function request(): Request
-	{
-		return $this->get(Request::class);
-	}
+    public function request(): Request
+    {
+        return $this->get(Request::class);
+    }
 
-	public function response(): Response
-	{
-		return $this->get(Response::class);
-	}
+    public function response(): Response
+    {
+        return $this->get(Response::class);
+    }
 }
